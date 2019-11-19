815963859b1f5779fba5181ad2698085e2d38f3a
Corentin <corentin.escoffier@gmail.com>
2019-11-19 20:17:06 +0100

    Update from live raspi

--- raspberrypi/usb_serial.py
+++ raspberrypi/usb_serial.py
@@ -1,1 +1,20 @@
-empty
+import serial
+import re
+from datetime import datetime
+
+pattern = re.compile("MSG://([a-z0-9\-]+);(C=[\+\-]+[0-9]+\.[0-9]+)");
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+while 1: 
+    if(ser.in_waiting >0):
+        line = ser.readline()[:-2]
+        match = pattern.match(line.decode('utf-8'))
+        if match:
+            rawMessage=match.group().replace("MSG://", "")
+            rewrittenMessage=rawMessage.split(';')[0]+";"+datetime.now().strftime("%d/%m/%Y")+";"+datetime.now().strftime("%H:%M:%S")+";"+rawMessage.split(';')[1]
+            print(rewrittenMessage)
+            f=open("/home/pi/Sensors/external_temperatures.log", "a")
+            f.write(rewrittenMessage + "\n")
+            f.close()
+        else:
+            print("No match for line='" + line.decode('utf-8') + "'")
